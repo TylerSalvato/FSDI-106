ade8d10d41392a34ded5f4ad5077cf12e36fc274
TylerSalvato <133399190+TylerSalvato@users.noreply.github.com>
2023-08-27 12:10:41 -0400

    Add files via upload

---
+++ 106/CH-40/scripts/task.js
@@ -0,0 +1,13 @@
+class Task {
+
+    constructor(isImportant,title,desc,category,dueDate,Priority)
+    {
+        this.isImportant = isImportant;
+        this.title = title;
+        this.description = desc;
+        this.category = category;
+        this.dueDate = dueDate;
+        this.Priority = Priority;
+    }
+
+}
